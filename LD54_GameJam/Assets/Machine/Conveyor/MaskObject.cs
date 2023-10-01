a7edb53022c71eec89a48293079b406f47b8c4da
Mister Bamboo <nicolascnaud1989@gmail.com>
2023-10-01 10:05:03 -0400

    feat: Move belt anim

---
+++ LD54_GameJam/Assets/Machine/Conveyor/MaskObject.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MaskObject : MonoBehaviour
+{
+    public GameObject[] gameObjects;
+
+    void Start()
+    {
+        foreach (GameObject go in gameObjects)
+        {
+            go.GetComponent<MeshRenderer>().material.renderQueue = 3002;
+        }
+    }
+}
