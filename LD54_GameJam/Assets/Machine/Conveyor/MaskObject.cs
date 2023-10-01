f05cf7ca0b990d06c6b72f1773520943cc68ec8b
Mister Bamboo <nicolascnaud1989@gmail.com>
2023-10-01 12:05:21 -0400

    feat: Create shaders

--- LD54_GameJam/Assets/Machine/Conveyor/MaskObject.cs
+++ LD54_GameJam/Assets/Machine/Conveyor/MaskObject.cs
@@ -6,9 +6,9 @@ public class MaskObject : MonoBehaviour
 
     void Start()
     {
-        foreach (GameObject go in gameObjects)
-        {
-            go.GetComponent<MeshRenderer>().material.renderQueue = 3002;
-        }
+        //foreach (GameObject go in gameObjects)
+        //{
+        //    go.GetComponent<MeshRenderer>().material.renderQueue = 3002;
+        //}
     }
 }
